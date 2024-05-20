ce8c67f609cf9b5f37f01ed90a1aa3c98166fb05
SangJLee1103 <jesusfollow1103@naver.com>
2024-05-20 15:21:10 +0900

    feat: Stack Study

---
+++ I Am Rich/I Am Rich/I_Am_RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_RichApp.swift
+//  I Am Rich
+//
+//  Created by 이상준 on 5/20/24.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
